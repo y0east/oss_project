8d3d72553a10cbc7783ccddd7cb846f20737f2b8
innew <innew@hufs.ac.kr>
2025-05-21 18:40:07 +0900

    backend initial commit

---
+++ backend/src/main/java/kr/ac/hufs/ice/ice/IceApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hufs.ice.ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceApplication.class, args);
+	}
+
+}
