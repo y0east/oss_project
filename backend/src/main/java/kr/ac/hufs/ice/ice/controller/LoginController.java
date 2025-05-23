5921447055ce39f22b72545285ae7b577973c1ed
innew <innew@hufs.ac.kr>
2025-05-23 13:58:51 +0900

    add Cors support with @CrossOrigin annotation in LoginController to connect with fronetend

--- backend/src/main/java/kr/ac/hufs/ice/ice/controller/LoginController.java
+++ backend/src/main/java/kr/ac/hufs/ice/ice/controller/LoginController.java
@@ -5,11 +5,10 @@ import jakarta.servlet.http.HttpSession;
 import kr.ac.hufs.ice.ice.dto.MemberDto;
 import kr.ac.hufs.ice.ice.service.LoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -17,11 +16,12 @@ public class LoginController {
 
     private final LoginService loginService;
 
+    // Todo refactoring
     @PostMapping("/login")
     public String login(@RequestBody MemberDto loginRequestDto, HttpSession session) {
         boolean success = loginService.login(loginRequestDto.getStudentId(), loginRequestDto.getPassword());
         if (success) {
-            session.setAttribute("studentId", loginRequestDto.getStudentId());
+            session.setAttribute("stuentId", loginRequestDto.getStudentId());
             return "로그인 성공";
         } else {
             return "로그인 실패: 학번 또는 비밀번호가 잘못되었습니다.";
